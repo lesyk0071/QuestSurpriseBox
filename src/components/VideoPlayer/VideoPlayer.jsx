ad54ae342165a5bc50e9b248d3badc8e25879558
Vitaliy Leskiv <vitaliy.leskiv@getinge.com>
2024-10-28 20:32:46 +0200

    playsInline added for a video

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import Operniy from "../../assets/Operniy.mp4";
-import { useNavigate } from 'react-router-dom';
-import './VideoPlayer.css'
+import { useNavigate } from "react-router-dom";
+import "./VideoPlayer.css";
 
 const VideoPlayer = () => {
   const videoRef = useRef(null);
@@ -22,28 +22,43 @@ const VideoPlayer = () => {
   };
 
   const handleClick = () => {
-    navigate('/prologue');  
+    navigate("/prologue");
   };
 
   return (
     <div className="main">
-        <div className="overlay"></div>
-            <video ref={videoRef} src={Operniy} autoPlay muted onEnded={handleVideoEnd} />
-            <div className="content">
-                <div className= {showContent ? "visible-text-first-part" : "hidden-text-first-part"}>
-                    <h1>Одного разу у Львiвськiй Оперi</h1>
-                </div>
-                <div className={showContent ? "visible-text-second-part" : "hidden-text-second-part"}>
-                    <p>Історія про загублену фотографiю</p>
-                </div>
-                <div className={showContent ? "visible-button" : "hidden-button"}>
-                    <div className="box-1" onClick={handleClick}>
-                        <div className="btn btn-one">
-                            <span>Почати</span>
-                        </div>
-                    </div>
-                </div>
+      <div className="overlay"></div>
+      <video
+        ref={videoRef}
+        src={Operniy}
+        autoPlay
+        muted
+        playsInline
+        onEnded={handleVideoEnd}
+      />
+      <div className="content">
+        <div
+          className={
+            showContent ? "visible-text-first-part" : "hidden-text-first-part"
+          }
+        >
+          <h1>Одного разу у Львiвськiй Оперi</h1>
+        </div>
+        <div
+          className={
+            showContent ? "visible-text-second-part" : "hidden-text-second-part"
+          }
+        >
+          <p>Історія про загублену фотографiю</p>
+        </div>
+        <div className={showContent ? "visible-button" : "hidden-button"}>
+          <div className="box-1" onClick={handleClick}>
+            <div className="btn btn-one">
+              <span>Почати</span>
             </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
